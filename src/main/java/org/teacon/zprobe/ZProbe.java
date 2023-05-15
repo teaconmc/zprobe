c1d1e2597da3281bb02d1289dbd9f8acf158d9bd
SeraphJACK <seraphjack@outlook.com>
2023-05-15 23:55:32 +0800

    Make display test ignore server only

--- src/main/java/org/teacon/zprobe/ZProbe.java
+++ src/main/java/org/teacon/zprobe/ZProbe.java
@@ -6,9 +6,12 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.server.ServerStartedEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLDedicatedServerSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.network.NetworkConstants;
 import org.slf4j.Logger;
 
 @Mod(ZProbe.ID)
@@ -26,6 +29,9 @@ public final class ZProbe {
 
     public void serverSetup(FMLDedicatedServerSetupEvent event) {
         logger.debug("ZProbe reached server setup");
+
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class,
+                () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
 
         boolean vanillaShutdownHookRemoved = false;
 
